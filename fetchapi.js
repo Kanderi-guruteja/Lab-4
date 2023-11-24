469d6a57bb9486231872940a28ca5f03f904b75b
Guruteja Kanderi <105558277+Kanderi-guruteja@users.noreply.github.com>
2023-11-24 17:37:42 -0600

    Update fetchapi.js

--- fetchapi.js
+++ fetchapi.js
@@ -1,54 +1,53 @@
-function searchLocation() {
-    const locationInput = document.getElementById('location');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Sunrise Sunset Web App</title>
+</head>
+<body>
+    <div class="container">
+        <img src="weather.jpeg" alt="Weather Image" class="weather-image">
+        <h1>Sunrise and Sunset Dashboard</h1>
+        <div class="location">
+            <label for="location">Location:</label>
+            <input type="text" id="location" placeholder="Enter location">
+            <button onclick="searchLocation()">Search</button>
+            <button onclick="getCurrentLocation()">Use Current Location</button>
+        </div>
+        <div class="options">
+            <label for="date">Select Date:</label>
+            <select id="date">
+                <option value="today">Today</option>
+                <option value="tomorrow">Tomorrow</option>
+            </select>
 
-    const location = locationInput.value;
+            <label for="event">Select Event:</label>
+            <select id="event">
+                <option value="sunrise">Sunrise</option>
+                <option value="sunset">Sunset</option>
+                <option value="dawn">Dawn</option>
+                <option value="dusk">Dusk</option>
+                <option value="dayLength">Day Length</option>
+                <option value="solarNoon">Solar Noon</option>
+            </select>
 
-    if (location.trim() === "") {
-        alert("Please enter a location");
-        return;
-    }
+            <button onclick="getData()">Get Data</button>
+        </div>
 
-    // Use the geocode API to get latitude and longitude for the named location
-    const geocodeUrl = `https://geocode.maps.co/?address=${encodeURIComponent(location)}`;
+        <div id="result" class="result"></div>
 
-    fetch(geocodeUrl)
-        .then(response => response.json())
-        .then(geocodeData => {
-            if (geocodeData.results.length > 0) {
-                const latitude = geocodeData.results[0].geometry.location.lat;
-                const longitude = geocodeData.results[0].geometry.location.lng;
-
-                // Now, you can use the latitude and longitude to fetch sunrise/sunset data
-                const dateSelect = document.getElementById('date');
-                const eventSelect = document.getElementById('event');
-                getData(latitude, longitude, dateSelect.value, eventSelect.value);
-            } else {
-                alert("Location not found. Please try again.");
-            }
-        })
-        .catch(error => {
-            console.error("Geocode API error:", error);
-            alert("Error fetching location data. Please try again.");
-        });
-}
-
-function getCurrentLocation() {
-    // Implement Geolocation API for getting current location
-    // Update latitude and longitude, then call getData()
-}
-
-function getData(latitude, longitude, date, event) {
-    const resultDiv = document.getElementById('result');
-
-    const apiUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&date=${date}&formatted=0`;
-
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Update the resultDiv with the relevant data
-            resultDiv.innerHTML = `<p>${event} time at ${latitude}, ${longitude} on ${date}: ${data.results[event]}</p>`;
-        })
-        .catch(error => {
-            resultDiv.innerHTML = `<p>Error: ${error.message}</p>`;
-        });
-}
+        <!-- Credits for APIs -->
+        <div class="credits">
+            <p>Resources:</p>
+            <ul>
+                <li><a href="https://sunrisesunset.io/" target="_blank">Sunrise Sunset API</a></li>
+                <li><a href="https://sunrisesunset.io/api/" target="_blank">Sunrise Sunset API Documentation</a></li>
+                <li><a href="https://geocode.maps.co/" target="_blank">Geocode API</a></li>
+            </ul>
+        </div>
+    </div>
+    <script src="fetchapi.js"></script>
+</body>
+</html>
